e9da4355d69f4d7e730b4fc81b000d2d1dba28d8
molero3111 <molero3111@gmail.com>
2024-08-09 12:24:12 -0400

    First commit

---
+++ src/components/Modal.jsx
@@ -0,0 +1,26 @@
+import { createPortal } from "react-dom";
+import { motion } from "framer-motion";
+
+export default function Modal({ title, children, onClose }) {
+  return createPortal(
+    <>
+      <div className="backdrop" onClick={onClose} />
+      <motion.dialog
+        variants={{
+          hidden: { opacity: 0, y: "10vh", backgroundColor: "#0f3cef" },
+          visible: { opacity: 1, y: "0", backgroundColor: "#fff" },
+        }}
+        initial="hidden"
+        animate="visible"
+        exit="hidden"
+        transition={{ duration: 0.6 }}
+        open
+        className="modal"
+      >
+        <h2>{title}</h2>
+        {children}
+      </motion.dialog>
+    </>,
+    document.getElementById("modal")
+  );
+}
---
+++ src/components/NewChallenge.jsx
@@ -0,0 +1,108 @@
+import { useContext, useRef, useState } from "react";
+import { motion, useAnimate, stagger } from "framer-motion";
+
+import { ChallengesContext } from "../store/challenges-context.jsx";
+import Modal from "./Modal.jsx";
+import images from "../assets/images.js";
+
+export default function NewChallenge({ onDone }) {
+  const title = useRef();
+  const description = useRef();
+  const deadline = useRef();
+  const [scope, animate] = useAnimate();
+
+  const [selectedImage, setSelectedImage] = useState(null);
+  const { addChallenge } = useContext(ChallengesContext);
+
+  function handleSelectImage(image) {
+    setSelectedImage(image);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const challenge = {
+      title: title.current.value,
+      description: description.current.value,
+      deadline: deadline.current.value,
+      image: selectedImage,
+    };
+
+    if (
+      !challenge.title.trim() ||
+      !challenge.description.trim() ||
+      !challenge.deadline.trim() ||
+      !challenge.image
+    ) {
+      animate(
+        "input, textarea",
+        { x: [-15, 0, 15, 0], backgroundColor: "#FFB2B2" },
+        { type: "spring", duration: 0.5, delay: stagger(0.2) }
+      );
+      return;
+    }
+
+    onDone();
+    addChallenge(challenge);
+  }
+
+  const listVariants = {
+    hidden: {},
+    visible: {
+      transition: {
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
+  const itemVariants = {
+    hidden: { opacity: 0, scale: 0.3 },
+    visible: { opacity: 1, scale: [0.7, 1.4, 1] },
+  };
+
+  return (
+    <Modal title="New Challenge" onClose={onDone}>
+      <form id="new-challenge" onSubmit={handleSubmit} ref={scope}>
+        <p>
+          <label htmlFor="title">Title</label>
+          <input ref={title} type="text" name="title" id="title" />
+        </p>
+
+        <p>
+          <label htmlFor="description">Description</label>
+          <textarea ref={description} name="description" id="description" />
+        </p>
+
+        <p>
+          <label htmlFor="deadline">Deadline</label>
+          <input ref={deadline} type="date" name="deadline" id="deadline" />
+        </p>
+
+        <motion.ul
+          variants={listVariants}
+          initial="hidden"
+          animate="visible"
+          id="new-challenge-images"
+        >
+          {images.map((image) => (
+            <motion.li
+              variants={itemVariants}
+              transition={{ type: "spring" }}
+              key={image.alt}
+              onClick={() => handleSelectImage(image)}
+              className={selectedImage === image ? "selected" : undefined}
+            >
+              <img {...image} />
+            </motion.li>
+          ))}
+        </motion.ul>
+
+        <p className="new-challenge-actions">
+          <button type="button" onClick={onDone}>
+            Cancel
+          </button>
+          <button>Add Challenge</button>
+        </p>
+      </form>
+    </Modal>
+  );
+}
